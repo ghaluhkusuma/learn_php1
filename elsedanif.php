4b240be7512644916f1afa0b141eb165a4d2946c
Ghaluh Kusuma <ghaluhkusuma21@gmail.com>
2022-11-09 19:36:46 +0700

    menabahkan else if

---
+++ elsedanif.php
@@ -0,0 +1,16 @@
+<?php
+
+$uang_programmer = 1000;
+$keyboard = 2000;
+$uang_designer = 3000;
+
+if($uang_designer > $keyboard){
+    echo "dibeli";
+
+}else if($uang_designer > $keyboard){
+    echo "dibeli";
+} else{
+    echo "nggag cukup";
+}
+
+?>

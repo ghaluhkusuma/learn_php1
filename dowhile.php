55e6d682c2458189f02cb08f706f8e44d9ee658a
Ghaluh Kusuma <ghaluhkusuma21@gmail.com>
2022-11-09 19:18:00 +0700

    menambahkan file while dan do while

---
+++ dowhile.php
@@ -0,0 +1,13 @@
+<?php 
+ $hewan = ['anjing','kuda','cicak','domba','buaya'];
+$i = 3;
+
+
+do{
+    echo $hewan[$i] . "<br>";
+    $i++;
+
+}while($i < count($hewan));
+
+
+?>
---
+++ while.php
@@ -0,0 +1,10 @@
+<?php 
+ $hewan = ['anjing','kuda','cicak','domba','buaya'];
+$i = 0;
+
+while($i < count($hewan)){
+    echo $hewan[$i] . "<br>";
+    $i++;
+}
+
+?>

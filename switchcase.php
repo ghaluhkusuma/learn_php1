9d73741034c29590183621b4c607cd3e56527a5b
Ghaluh Kusuma <ghaluhkusuma21@gmail.com>
2022-11-10 10:31:26 +0700

    membuat true false

---
+++ switchcase.php
@@ -0,0 +1,15 @@
+<?php
+$nama ="Hilman";
+switch ($nama){
+    case "Endy":
+        echo "namanya endy";
+
+        break;
+    case "Tiqa":
+        echo "namanya tiqa";
+    case "Hilman":
+        echo "namanya Hilman";
+        break;
+}
+
+?>
